f9dc596e8b37a1045bf758cf97d95bcfc695abf3
7AtAri <83241462+7AtAri@users.noreply.github.com>
2022-12-06 18:21:02 +0100

    Update build.py

--- build.py
+++ build.py
@@ -6,8 +6,11 @@ use_plugin("python.coverage")
 use_plugin("python.distutils")
 ## use_plugin("python.install_dependencies")
 
+name = "Appointment Bot"
 default_task = "publish"
 
-# @init
-# def set_properties(project):
-#     pass
+@init
+def set_properties(project):
+  project.set_property("coverage_break_build", FALSE)
+  # project.depends_on("unittest") etc. 
+
