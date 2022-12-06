6b0e85857aaf0eb250acec647d9b088c512fc108
7AtAri <83241462+7AtAri@users.noreply.github.com>
2022-12-06 18:09:31 +0100

    Update build.py

--- build.py
+++ build.py
@@ -4,5 +4,10 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+## use_plugin("python.install_dependencies")
 
 default_task = "publish"
+
+# @init
+# def set_properties(project):
+#     pass
