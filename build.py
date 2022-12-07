06e0b7d8aee271ece6b33f1f4825cc66cc19d448
7AtAri <7AtAri@users.noreply.github.com>
2022-12-07 21:32:53 +0100

    bug fix

--- build.py
+++ build.py
@@ -1,16 +1,17 @@
-from pybuilder.core import use_plugin
+from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-## use_plugin("python.install_dependencies")
+use_plugin('python.pycharm')
+# use_plugin("python.install_dependencies")
 
 name = "Appointment Bot"
 default_task = "publish"
 
+
 @init
 def set_properties(project):
-  project.set_property("coverage_break_build", FALSE)
-  # project.depends_on("unittest") etc. 
-
+    project.set_property("coverage_break_build", False)
+    # project.depends_on("unittest") etc.
