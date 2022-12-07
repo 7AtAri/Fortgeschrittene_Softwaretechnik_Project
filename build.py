a51efb2b777bedbe625ec210aaf9cbe9a3e48970
7AtAri <7AtAri@users.noreply.github.com>
2022-12-07 22:05:33 +0100

    pybuilder refactoring

--- build.py
+++ build.py
@@ -4,11 +4,18 @@ use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-use_plugin('python.pycharm')
+use_plugin('pypi:pybuilder_pycharm_workspace')
+# use_plugin('python.pycharm')
 # use_plugin("python.install_dependencies")
 
 name = "Appointment Bot"
 default_task = "publish"
+
+
+@init
+def initialise(project):
+    project.set_property('pycharm_workspace_main_version', '2019')
+    project.set_property('pycharm_workspace_project_path', project_path)
 
 
 @init
