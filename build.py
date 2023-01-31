6145d26c229844748dca6f8a0c03e546e3572019
7AtAri <83241462+7AtAri@users.noreply.github.com>
2023-01-31 20:19:43 +0100

    Update build.py

--- build.py
+++ build.py
@@ -14,19 +14,16 @@ use_plugin('pypi:pybuilder_pycharm_workspace')
 project_path = Path(__file__).resolve().parent
 
 name = project_path.name
-authors = [Author("foo", 'bar')]
-# license = "Apache License, Version 2.0"
-# version = '1.0.0'
 default_task = "publish"
 
 
 @init
 def set_properties(project):
+    project.depends_on('unittest') #?
     project.set_property('pycharm_workspace_project_path', project_path)
     project.set_property("coverage_break_build", False)
 
 
-# Most important part of the script below (previous one is just filling code)
 if __name__ == '__main__':
     import sys
 
