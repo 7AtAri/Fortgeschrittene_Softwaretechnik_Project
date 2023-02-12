d0c6fcbcf1cffa979b5a404d662877aebc88ed67
7AtAri <7AtAri@users.noreply.github.com>
2023-02-12 17:05:11 +0100

    refactor

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -77,8 +77,8 @@ def schedule(user_info1, appointment_wish1, bot_search_interval1):
         bot.use_element("click", browser, "agbgelesen", By.ID)
 
         # bot submits the appointment registration:
-        # !!! only comment in if you really want to book an appointment:
-        # bot.use_element("click", browser, "register_submit", By.ID)
+        # !!! out comment in if you don't really want to book an appointment:
+        bot.use_element("click", browser, "register_submit", By.ID)
 
         browser.quit()  # selenium browser object is shut down
     else:
