75792c5ef8f2dd56606af261d6ec6b14b5808407
7AtAri <7AtAri@users.noreply.github.com>
2023-02-12 16:29:01 +0100

    Domain Specific language demo, etc

--- DSL/morse_code_telegraph.py
+++ DSL/morse_code_telegraph.py
@@ -7,32 +7,34 @@ from textx import metamodel_from_file
 import pandas as pd
 import string
 
-
 morse_meta = metamodel_from_file("morse.tx", debug=True)
 
 morse_code_text = morse_meta.model_from_file("telegraph.morse")
 
 latin_alphabet = list(string.ascii_lowercase)
 
-morse_code=['.-', '-...', '-.-.', '-..' , '.' , \
-            '..-.' , '--.' , '....' , '..' , '.---' ,\
-            '-.-' , '.-..' , '--' , '-.' , '---' , '.--.' ,\
-            '--.-' , '.-.' , '...' , '-' , '..-' , '...-' , \
-            '.--' , '-..-' , '-.--' , '--..']
+morse_code = ['.-', '-...', '-.-.', '-..', '.',
+              '..-.', '--.', '....', '..', '.---',
+              '-.-', '.-..', '--', '-.', '---', '.--.',
+              '--.-', '.-.', '...', '-', '..-', '...-',
+              '.--', '-..-', '-.--', '--..']
 
 translation_df = pd.DataFrame(list(zip(latin_alphabet, morse_code)),
-               columns =['Latin', 'Morse'])
+                              columns=['Latin', 'Morse'])
 
-translation=[]
-def translate(morse_code_text):
-    for sents in morse_code_text.translate_instructions:
+translation = []
+
+
+def translate(morse_code_text1):
+    for sents in morse_code_text1.translate_instructions:
         for words in sents.sentence.word:
-            letter_list=str(*words.morse).split('+')
+            letter_list = str(*words.morse).split('+')
             for a_letter in letter_list:
-                df_row_idx=translation_df.index[translation_df['Morse']==a_letter]
-                idx=df_row_idx.tolist()[0]
+                df_row_idx = translation_df.index[translation_df['Morse'] == a_letter]
+                idx = df_row_idx.tolist()[0]
                 translation.append(translation_df.at[idx, 'Latin'])
             translation.append(' ')
+
 
 translate(morse_code_text)
 
